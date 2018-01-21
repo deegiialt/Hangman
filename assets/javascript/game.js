bea1aff278ddc406acfe016c08c697358ee7d1ab
DeegiiAlt <aagiideegii@gmail.com>
2018-01-20 18:24:15 -0700

    added javascript pseudocoding

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+//Define var for array containing objects for words and their letters (each word followed by 
+//their letters is a new object)
+
+var wordsToBeGuessed = [
+	{
+		word: ,
+		letters: ,
+	},
+	{
+		word: ,
+		letters: ,
+	},
+	{
+		word: ,
+		letters: ,
+	},
+	{
+		word: ,
+		letters: ,
+	},
+]
+
+//Define var for wins, current word, guesses remaining, already guessed, pics of guess after each win
+
+//Create functions to render words to be guessed. if/else statement in function (if keep going,
+// else stop game)
+
+//Create function that keeps score of wins.
+
+//if a letter is guessed already, guesses don't go down.
+
+//MAIN PROCESS
+//Call functions to start game
+//When key is pressed start game (document.keyup)
+//if no more words to guess, stop w return
+
+//if the user input === to any of the letters, show the letter in current word,
+// ++wins,
+//else --guessesRemaining, show key typed in already guessed letters
+
+//Increment each word to be guessed, render.
